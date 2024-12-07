64a9a2fbe03085df3355f51d00e4432d92e9af3a
owenmorgan <owenpmorgan@gmail.com>
2024-12-07 17:11:31 +0000

    developed display and interface class

--- main.cpp
+++ main.cpp
@@ -130,7 +130,7 @@ using json = nlohmann::json;
  * The Following block is all just parsing the scraped json file and adding it to the various classes
  *****************************************************************************************************************/
 // open the json and parse the data to a json object called data
-    std::ifstream myFile("recipes.json");
+    std::ifstream myFile("recipes_clean.json");
     if (!myFile.is_open()) {
         std::cerr << "Failed to open JSON file." << std::endl;
         return 1;
